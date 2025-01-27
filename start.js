b1d7cc4835a9aef9ccefb7ef64f6cc6180f0486b
ozan192 <ozan59222@gmail.com>
2025-01-27 21:58:58 +0300

    Create start.js

---
+++ start.js
@@ -0,0 +1,10 @@
+const { exec } = require('child_process');
+
+exec('./target/release/url-sniper', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`exec error: ${err}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
